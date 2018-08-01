79f001d1bdf96fb6a399fea9cd1f1515e5c081ba
Richard Fry <Rich@richfry.com>
2018-08-01 09:12:28 -0600

    Final after class 8/1

--- TinyUniversity/Pages/Instructors/Index.cshtml.cs
+++ TinyUniversity/Pages/Instructors/Index.cshtml.cs
@@ -19,14 +19,19 @@ namespace TinyUniversity.Pages.Instructors
 
         public InstructorIndexData Instructor { get; set; }
         public int InstructorID { get; set; }
-
-        public async Task OnGetAsync(int? id)
+        public int CourseID { get; set; }
+        public async Task OnGetAsync(int? id, int? courseID)
         {
             Instructor = new InstructorIndexData();
             Instructor.Instructors = await _context.Instructor
                   .Include(i => i.OfficeAssignment)
                   .Include(i => i.CourseAssignments)
                     .ThenInclude(i => i.Course)
+                    .ThenInclude(i => i.Department)
+                .Include(i => i.CourseAssignments)
+                  .ThenInclude(i => i.Course)
+                  .ThenInclude(i => i.Enrollments)
+                  .ThenInclude(i => i.Student)
                   .AsNoTracking()
                   .OrderBy(i => i.LastName)
                   .ToListAsync();
@@ -34,6 +39,17 @@ namespace TinyUniversity.Pages.Instructors
             if (id != null)
             {
                 InstructorID = id.Value;
+                Instructor instructor = Instructor.Instructors.Where(
+                    i => i.ID == id.Value).Single();
+                Instructor.Courses = instructor.CourseAssignments.Select(s => s.Course);
+            }
+
+            if (courseID != null)
+            {
+                CourseID = courseID.Value;
+                Instructor.Enrollments = Instructor.Courses.Where(
+                    x => x.CourseID == CourseID).Single().Enrollments;
+               
             }
         }
     }
